7be54b7166469636645c8672b83ce418e92921aa
jakir442 <jakigeming2021@gmail.com>
2024-07-14 16:41:49 +0700

    payment melalui chat whatsapp

--- src/app.js
+++ src/app.js
@@ -125,11 +125,12 @@ checkoutButton.addEventListener("click", function (e) {
 
 //format pesan whatsapp
 const formatMessage = (obj) => {
-  return `Data Customer :
+  return `YummyHub.
+  - Data Customer -
   Nama: ${obj.name}
   Email: ${obj.email}
   No.HP: ${obj.phone}
-  Data Pesanan :
+  - Data Pesanan -
   ${JSON.parse(obj.items).map(
     (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
   )}
